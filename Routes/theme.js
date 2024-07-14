c27c9c7df8ae42da379a52a5cc5fbf044b3dcea8
Muhammad3490 <muhg56190@gmail.com>
2024-07-14 02:15:05 -0700

    Updated folder name

--- Routes/theme.js
+++ Routes/theme.js
@@ -4,7 +4,7 @@ const multer  = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'themeUploads');
+        cb(null, 'ThemeUploads');
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}-${file.originalname}`); // corrected from file.filename to file.originalname
